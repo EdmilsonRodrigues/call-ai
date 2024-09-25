b9ba1418f8eb6fac6e7a0deaf0fc2331f591d718
Edmilson Monteiro Rodrigues Neto <e.rodrigues@roborian.com>
2024-09-25 20:59:51 +0100

    Update python basic logic

---
+++ python_logic.py
@@ -0,0 +1,17 @@
+# Comments are made with #
+a = "45"
+A = 12
+a_A = 45
+
+# variables are case sensitive. Variables can't use - only _
+
+# Python use snake_case to variables and functions and CapitalCamelCase to classes
+
+# Base data structures are lists, ducts, sets and tupples
+
+tupla = (1, 2)  # Immutable 
+lista = [1, 2]  # Muttable 
+dicionário = {"A": 1, "B": 2}  # Key-Value pairs 
+conjunto = {1, 2}  # Unique values
+
+
