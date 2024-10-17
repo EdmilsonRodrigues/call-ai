9ff031812677a80aee6a5b0d8c3dcafc7688a2f9
Edmilson Rodrigues <edmilson.rodriguesn38@gmail.com>
2024-10-17 07:58:15 +0100

    Working on Router

--- routers/twilio.py
+++ routers/twilio.py
@@ -14,21 +14,33 @@ async def media_stream(websocket: WebSocket):
 
     try:
         while True:
-            data = await websocket.receive_text()
-            
-            response_audio = generate_response_audio()
-            
-            await websocket.send_bytes(response_audio)
+            send_task  = asyncio.create_task(send_audio_chunks(websocket))
+            receive_task = asyncio.create_task(receive_customer_audio(websocket))
 
+            done, pending = await asyncio.wait(
+                    {send_task, receive_task}, return_when=asyncio.FIRST_COMPLETED
+                    )
+
+            for task in pending:
+                task.cancel()
+
+            if receive_task in done:
+                customer_data = await receive_task
+
+                await handle_customer_input(customer_data)
+                
     except websockets.ConnectionClosedError:
         print("Connection Closed")
 
+    finally:
+        await websocket.close()
 
-@app.post("/receive-audio/")
+
+@router.post("/receive-audio/")
 async def twilio_voice():
     response = VoiceResponse()
     response.say("Say something")
-    response.start().stram(url=f"wss://{BASE_API_URL}/media-stream")
+    response.start().stream(url=f"wss://{BASE_API_URL}/media-stream")
     return PlainTextResponse(str(response), media_type="application/xml")
 
 def generate_response_audio():
